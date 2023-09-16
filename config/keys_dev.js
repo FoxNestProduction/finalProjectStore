872a5678bd2ad3ddb38f259c66e0ac5d68f1238b
Stephan <s.dumenko@icloud.com>
2023-09-16 12:16:51 +0200

    fix: route mongoDB fixed

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI:
-    '//admin:Xzwxj5fM4sxm1Z1F@cluster0.me12bv8.mongodb.net/foodtoorder?retryWrites=true&w=majority',
+    'mongodb+srv://admin:Xzwxj5fM4sxm1Z1F@cluster0.me12bv8.mongodb.net/foodtoorder?retryWrites=true&w=majority',
   secretOrKey: 'Xzwxj5fM4sxm1Z1F',
 };
