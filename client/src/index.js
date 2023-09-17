cdc2a99698e84a097466314ad681d16d46d55dce
Stephan <s.dumenko@icloud.com>
2023-09-17 19:49:53 +0200

    feat: add new rules ESlint

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));// eslint-disable-line no-undef
 root.render(
   <React.StrictMode>
     <App />
