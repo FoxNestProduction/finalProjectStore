f0f2d884d273e7294c69b4079cf7ffc3a711ce8c
romadzhavadov <r.dzhavadov12@gmail.com>
2023-09-19 16:43:36 +0300

    add redux/toolkit

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import './reset.scss';
 import App from './App';
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root')); // eslint-disable-line no-undef
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
 );
