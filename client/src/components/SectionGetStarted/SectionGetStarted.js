1cbe4b6dcb593e022efb8f07df956083de5e5ce7
Ihor Kacher <ihorkacher@gmail.com>
2023-09-24 10:52:03 +0200

    wip

---
+++ client/src/components/SectionGetStarted/SectionGetStarted.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './getStarted.module.scss';
+
+const SectionGetStarted = () => {
+  return (
+    <section className={styles.getStarted}>
+      <h1 className="styles.getStarted__title">Enjoy Foods All Over The World</h1>
+    </section>
+  );
+};
+
+export default SectionGetStarted;
