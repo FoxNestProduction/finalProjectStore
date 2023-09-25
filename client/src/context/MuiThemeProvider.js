90ca0ecd85d66bee16294bdfaba3f2bbdf365934
Kovtun Oksana <pavoks962@gmail.com>
2023-09-26 00:19:22 +0300

    settings theme breakpoints colors fonts

--- client/src/context/MuiThemeProvider.js
+++ client/src/context/MuiThemeProvider.js
@@ -5,62 +5,121 @@ import PropTypes from 'prop-types';
 const MuiThemeProvider = ({ children }) => {
   const theme = createTheme({
     breakpoints: {
+      keys: {
+        0: zero,
+        1: mobile,
+        2: tablet,
+        3: desktop,
+        4: screen,
+      },
       values: {
+        zero: 0,
         mobile: 320,
         tablet: 481,
         desktop: 993,
+        screen: 1200,
       },
     },
     palette: {
       primary: {
         main: '#6C5FBC',
+        hover: '#664FFF',
       },
-      primaryDefaultColor: {
-        main: '#6C5FBC',
-        second: '#FF9140',
+      secondary: {
+        main: '#FF9140',
+        hover: '#FF7C1C',
       },
-      primaryHoverColor: {
-        main: '#664FFF',
-        second: '#FF7C1C',
+      common: {
+        white: '#F9F9F9',
       },
-      labelColor: {
-        healthy: '#DAA31A',
-        trending: '#FB471D',
-        supreme: '#309D5B',
-      },
-      textColor: {
-        main: '#2B2B2B',
+      text: {
+        primary: '#2B2B2B',
+        secondary: '#2B2B2B',
         header: '#606060',
         footer: '#969696',
-        title: '#201F1F',
-        subTitile: '#323142',
       },
-      healthyColor: {
-        background: '#F7EDD0',
-        text: '#DAA31A',
+      background: {
+        paper: '#F9F9F9',
+        default: '#EFEFEF',
       },
-      trendingColor: {
-        background: '#F7C5BA',
-        text: '#FB471D',
-      },
-      supremeColor: {
-        background: '#00B67A',
-        text: '#309D5B',
-      },
+      healthy: '#DAA31A',
+      trending: '#FB471D',
+      supreme: '#309D5B',
+      bgHealthy: '#F7EDD0',
+      bgTrending: '#F7C5BA',
+      bgSupreme: '#33AC64',
     },
     shape: {
       borderRadius: 12,
     },
     typography: {
-      fontInter: 'Inter, sans-serif',
-      fontFamily: 'Poppins, sans-serif',
-      fontWeightSemiBold: 600,
-      fontWeightExtraBold: 800,
-      fontSize: 14,
-      fontWeightLight: 300,
-      fontWeightRegular: 400,
-      fontWeightMedium: 500,
-      fontWeightBold: 700,
+      fontFamily: 'Inter, sans-serif',
+      body1: {
+        fontFamily: 'Inter, sans-serif',
+        fontWeight: 400,
+      },
+      body2: {
+        fontFamily: 'Inter, sans-serif',
+        fontWeight: 400,
+      },
+      h1: {
+        color: '#201F1F',
+      },
+      h2: {
+        color: '#323142',
+      },
+      button: {
+        fontFamily: 'Inter, sans-serif',
+        fontWeight: 500,
+      },
+      caption: {
+        fontFamily: 'Poppins, sans-serif',
+        fontWeight: 500,
+      },
+      h1: {
+        color: '#323142',
+        fontFamily: 'Poppins, sans-serif',
+        fontWeight: 500,
+      },
+      h2: {
+        color: '#2B2B2B',
+        fontFamily: 'Poppins, sans-serif',
+        fontWeight: 500,
+      },
+      h3: {
+        color: '#2B2B2B',
+        fontFamily: 'Poppins, sans-serif',
+        fontWeight: 500,
+      },
+      subtitle1: {
+        fontFamily: 'Inter, sans-serif',
+      },
+      subtitle2: {
+        fontFamily: 'Inter, sans-serif',
+      },
+      // fontInter: 'Inter, sans-serif',
+      // fontFamily: 'Poppins, sans-serif',
+      // fontWeightSemiBold: 600,
+      // fontWeightExtraBold: 800,
+      // fontSize: 14,
+      // fontWeightLight: 300,
+      // fontWeightRegular: 400,
+      // fontWeightMedium: 500,
+      // fontWeightBold: 700,
+    },
+    button: {
+      large: {
+        height: '76px',
+      },
+      medium: {
+        height: '60px',
+      },
+      small: {
+        height: '46px',
+      },
+      extraSmall: {
+        height: '35px',
+      },
     },
     components: {
       MuiFormControl: {
