d9e9862813f5a9dccee3201c98899e70592dbcd0
Ihor Kacher <ihorkacher@gmail.com>
2023-09-26 21:03:00 +0200

    WIP: rewrited SectionGetStarted component with MUI, without styles

--- client/src/components/SectionGetStarted/SectionGetStarted.js
+++ client/src/components/SectionGetStarted/SectionGetStarted.js
@@ -1,36 +1,44 @@
 import React from 'react';
-import { Rating, Typography } from '@mui/material';
-import { Link } from 'react-router-dom';
-import styles from './getStarted.module.scss';
+import { Typography, Link, Container, Box, Rating } from '@mui/material';
+import { Link as RouterLink } from 'react-router-dom';
 
 const SectionGetStarted = () => {
   const value = 4;
+
   return (
-    <section className={`container ${styles.getStarted}`}>
-      <span className={styles.getStarted__usersLabel}>OVER 1000 USERS</span>
-      <h1 className={styles.getStarted__title}>
+    <Container
+      component="section"
+    >
+      <Typography component="span">
+        OVER 1000 USERS
+      </Typography>
+      <Typography component="h1">
         Enjoy Foods All
         <br />
         Over The World
-      </h1>
-      <p className={styles.getStarted__description}>
+      </Typography>
+      <Typography component="p">
         {/* eslint-disable-next-line max-len */}
-        Eatly help you set saving goals, earn cash back offers, Go to disclaimer for more details and get paychecks up to two days early. Get a $20 bonus.
-      </p>
-      <div className={styles.getStarted__linksWrapper}>
-        <Link className={styles.getStarted__linksWrapper_getStartedLink} to="/menu">Get Started</Link>
-        <Link className={styles.getStarted__linksWrapper_goProLink} to="/Pricing">Go Pro</Link>
-      </div>
-      <div className={styles.getStarted__rating}>
+        Eatly helps you set saving goals, earn cash back offers, and get paychecks up to two days early. Get a $20 bonus.
+      </Typography>
+      <Box>
+        <Link component={RouterLink} to="/menu">
+          Get Started
+        </Link>
+        <Link component={RouterLink} to="/Pricing">
+          Go Pro
+        </Link>
+      </Box>
+      <Box>
         <img src="/img/layout/trustpilotIcon.png" alt="" />
-        <span className={styles.getStarted__rating_trustpilot}>Trustpilot</span>
+        <Typography component="span">Trustpilot</Typography>
         <Rating name="read-only" value={value} readOnly />
-        <span className={styles.getStarted__rating_numberOfReviews}>4900+</span>
-      </div>
-      <div className={styles.getStarted__imgWrapper}>
+        <Typography component="span">4900+</Typography>
+      </Box>
+      <Box>
         <img src="/img/layout/getStartedSectionImage.jpg" alt="" />
-      </div>
-    </section>
+      </Box>
+    </Container>
   );
 };
 
