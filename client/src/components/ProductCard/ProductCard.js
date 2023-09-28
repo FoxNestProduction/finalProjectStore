4a4bf37472f5cccf7fe112a5b6664b637fe9deef
Kovtun Oksana <pavoks962@gmail.com>
2023-09-28 21:35:20 +0300

    finished styling mobile

--- client/src/components/ProductCard/ProductCard.js
+++ client/src/components/ProductCard/ProductCard.js
@@ -8,6 +8,8 @@ import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
 
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 import AddBoxOutlinedIcon from '@mui/icons-material/AddBoxOutlined';
@@ -33,51 +35,47 @@ const ProductCard = () => {
           //  height=""
           image="./img/seaFood/norwegian_lobster.png"
           alt="lobster"
+          sx={{ mb: 3 }}
         />
-        <Grid container spacing={2}>
-          <Grid item>
-            Healsy
-          </Grid>
-          <Grid item>
-            <Typography variant="subtitle1">
-              24min •
-            </Typography>
-          </Grid>
-          <Grid item>
+        <Stack direction="row" spacing={2} sx={{ justifyContent: 'flex-end' }}>
+          <Typography variant="subtitle1">
+            24min •
+          </Typography>
+          <Stack direction="row" spacing={1}>
             <StarRoundedIcon sx={{ color: 'primary.main' }} />
-          </Grid>
-          <Grid item>
             <Typography variant="subtitle1">
               4.2
             </Typography>
-          </Grid>
-        </Grid>
-        <CardContent>
-          <Typography
-            variant="description"
-            sx={{ px: 0 }}
-          >
+          </Stack>
+        </Stack>
+        <CardContent sx={{ px: 0 }}>
+          <Typography variant="description">
             This impressive paella is a perfect party dish and a fun meal to cook
             together with your guests. Add 1 cup of frozen peas along with the mussels,
             if you like
           </Typography>
         </CardContent>
+        <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+          <Typography variant="h5">
+            $12.99
+          </Typography>
+        </Box>
         <CardActions
           sx={{ spacing: 8, justifyContent: ' space-between' }}
         >
           <Button
             variant="outlined"
-            sx={{ px: 3, py: 2, borderRadius: '12px', color: 'text.primary', border: 1 }}
+            sx={{ px: 3, py: 2, borderRadius: '14px', color: 'text.primary', border: 1 }}
           >
             Favourite
             <FavoriteBorderOutlinedIcon fontSize="medium" sx={{ ml: 1 }} />
           </Button>
           <Button
             variant="contained"
-            sx={{ px: 3, py: 2, borderRadius: '12px' }}
+            sx={{ px: 3, py: 2, borderRadius: '14px' }}
           >
             Add to card
-            <AddBoxOutlinedIcon fontSize="lmedium" sx={{ ml: 1 }} />
+            <AddBoxOutlinedIcon fontSize="medium" sx={{ ml: 1 }} />
           </Button>
         </CardActions>
       </Card>
