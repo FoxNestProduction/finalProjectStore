58ea15051e6eee5fe04716c15dade634daa701e1
dmitriykud <0955319181d@gmail.com>
2023-09-29 14:29:10 +0300

    fix style add secondary fontfamily to globalTheme

--- client/src/components/Footer/styles.js
+++ client/src/components/Footer/styles.js
@@ -3,44 +3,36 @@ export const stylesLink = {
   fontFamily: 'fontFamily.main',
   fontWeight: 'fontWeight.semiBold',
   alignSelf: {
-    zero: 'start',
     mobile: 'start',
     tablet: 'center',
-    desktop: 'center',
   },
   textDecoration: 'none',
   textWrap: 'nowrap',
   p: 0,
   pl: {
-    zero: '10px',
     mobile: '10px',
     tablet: 0,
   },
   minWidth: '0px',
   fontSize: {
-    zero: 14,
     mobile: 14,
     tablet: 12,
     desktop: 18,
   },
   textTransform: {
-    zero: 'uppercase',
     mobile: 'uppercase',
     tablet: 'none',
-    desktop: 'none',
   },
 };
 
 export const stylesWrap = {
   width: '100%',
   pt: {
-    zero: '55px',
     mobile: '55px',
     tablet: '69px',
     desktop: '96px',
   },
   pb: {
-    zero: '36px',
     mobile: '36px',
     tablet: '57px',
     desktop: '80px',
@@ -54,14 +46,12 @@ export const stylesTopWrap = {
 
 export const stylesLinkWrap = {
   gap: {
-    zero: '35px',
     mobile: '35px',
     tablet: '5vw',
     desktop: '6vw',
   },
   alignItems: 'start',
   mt: {
-    zero: '53px',
     mobile: '53px',
     tablet: 0,
     desktop: 0,
@@ -70,21 +60,17 @@ export const stylesLinkWrap = {
 
 export const stylesText = {
   alignSelf: {
-    zero: 'center',
     mobile: 'center',
     tablet: 'start',
-    desktop: 'start',
   },
   color: 'text.header',
   fontWeight: '500',
   fontSize: {
-    zero: '14px',
     mobile: '14px',
     tablet: '12px',
     desktop: '18px',
   },
   mt: {
-    zero: '26px',
     mobile: '26px',
     tablet: '41px',
     desktop: '57px',
@@ -93,7 +79,6 @@ export const stylesText = {
 
 export const stylesLine = {
   mt: {
-    zero: '33px',
     mobile: '33px',
     tablet: '20px',
     desktop: '29px',
@@ -106,14 +91,12 @@ export const stylesLine = {
 export const stylesSocialWrap = {
   position: 'absolute',
   top: {
-    zero: '65px',
     mobile: '65px',
     tablet: '170px',
     desktop: '240px',
   },
   right: '10px',
   gap: {
-    zero: '18px',
     mobile: '18px',
     tablet: '4vw',
     desktop: '51px',
@@ -127,9 +110,7 @@ export const stylesSocial = {
 
 export const stylesTwitter = {
   color: {
-    zero: '#201F1F',
     mobile: '#201F1F',
-    tablet: '#201F1F',
     desktop: 'primary.main',
   },
   p: '0',
@@ -137,9 +118,7 @@ export const stylesTwitter = {
 
 export const stylesSizeIcon = {
   fontSize: {
-    zero: '18px',
     mobile: '18px',
-    tablet: '18px',
     desktop: '21px',
   },
 };
--- client/src/components/Logo/styles.js
+++ client/src/components/Logo/styles.js
@@ -2,13 +2,11 @@ export const stylesIcon = {
   p: 0,
   mr: '10px',
   width: {
-    zero: 40,
     mobile: 40,
     tablet: 44,
     desktop: 62,
   },
   height: {
-    zero: 37,
     mobile: 37,
     tablet: 40,
     desktop: 57,
@@ -20,7 +18,6 @@ export const stylesTitle = {
   fontFamily: 'fontFamily.secondary',
   fontWeight: 'fontWeight.bold',
   fontSize: {
-    zero: 18,
     mobile: 18,
     tablet: 20,
     desktop: 29,
