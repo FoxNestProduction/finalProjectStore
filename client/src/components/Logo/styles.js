2eaad3dbbca91996954ef008559cc0fdd5a57dad
dmitriykud <0955319181d@gmail.com>
2023-09-29 14:09:36 +0300

    fix

---
+++ client/src/components/Logo/styles.js
@@ -0,0 +1,28 @@
+export const stylesIcon = {
+  p: 0,
+  mr: '10px',
+  width: {
+    zero: 40,
+    mobile: 40,
+    tablet: 44,
+    desktop: 62,
+  },
+  height: {
+    zero: 37,
+    mobile: 37,
+    tablet: 40,
+    desktop: 57,
+  },
+};
+
+export const stylesTitle = {
+  color: 'primary.main',
+  fontFamily: 'fontFamily.secondary',
+  fontWeight: 'fontWeight.bold',
+  fontSize: {
+    zero: 18,
+    mobile: 18,
+    tablet: 20,
+    desktop: 29,
+  },
+};
--- client/src/components/ProductItem/ProductItem.js
+++ client/src/components/ProductItem/ProductItem.js
@@ -2,13 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const ProductItem = () => {
-
-
   return (
     <div>ProductItem</div>
-  )
-}
+  );
+};
 
 // ProductItem.propTypes = {}
 
-export default ProductItem
+export default ProductItem;
