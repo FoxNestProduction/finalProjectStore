db0692b01a9fe7f9fd6e51a651d01b18f3f9c4d2
romadzhavadov <r.dzhavadov12@gmail.com>
2023-10-03 00:46:09 +0300

    work with  container

--- client/src/components/ListItems/ListItem.js
+++ client/src/components/ListItems/ListItem.js
@@ -4,13 +4,14 @@ import Typography from '@mui/material/Typography';
 import PropTypes from 'prop-types';
 import Grid from '@mui/material/Grid';
 import Divider from '@mui/material/Divider';
+import { gridStylesRestaurant } from './styles';
 
 import RestaurantItem from '../RestaurantItem/RestaurantItem';
 import ArrowIcon from '../../assets/svgComponents/ArrowIcon';
 
 const ListItems = ({ title, actions }) => {
   return (
-    <Container sx={{ mb: 18 }}>
+    <Container sx={{ mb: 13 }}>
       <Typography
         variant="h2"
         component="h2"
@@ -18,19 +19,19 @@ const ListItems = ({ title, actions }) => {
       >
         {title}
       </Typography>
-      <Grid container spacing={4} sx={{ mb: 8, m: 0, justifyContent: 'space-between' }}>
-        <Grid item mobile={6} desktop={3} sx={{ p: 0 }}>
+      <Grid container spacing={3}>
+        <Grid item mobile={12} lgTablet={6} desktop={4} sx={gridStylesRestaurant}>
           <RestaurantItem />
         </Grid>
-        <Grid item mobile={6} desktop={3} sx={{ p: 0 }}>
+        <Grid item mobile={12} lgTablet={6} desktop={4} sx={gridStylesRestaurant}>
           <RestaurantItem />
         </Grid>
-        <Grid item mobile={6} desktop={3} sx={{ p: 0 }}>
+        <Grid item mobile={12} lgTablet={6} desktop={4} sx={gridStylesRestaurant}>
           <RestaurantItem />
         </Grid>
-        <Grid item mobile={6} desktop={3} sx={{ p: 0 }}>
+        {/* <Grid item mobile={6} desktop={3} sx={{ p: 0 }}>
           <RestaurantItem />
-        </Grid>
+        </Grid> */}
         {/* {data.map((item) => (
           <Grid key={item.id} item mobile={6} desktop={3}>
             {item}
---
+++ client/src/components/ListItems/styles.js
@@ -0,0 +1,10 @@
+export const chipSizeDishes = {
+  width: '70px',
+
+};
+
+export const gridStylesRestaurant = {
+  display: 'flex',
+  justifyContent: 'center',
+  gap: '10px',
+};
