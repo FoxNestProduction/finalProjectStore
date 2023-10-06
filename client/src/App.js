6bde8effd3741aeae9edf3102ed94cf4f0561a20
Anna Shtendera <ann.shtendera@gmail.com>
2023-10-06 13:52:14 +0300

    feat: add scroll restoration up to top when path changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import './App.scss';
+import { useLocation } from 'react-router';
 import AppRoutes from './AppRoutes';
 import Modal from './components/Modal/Modal';
 import ScrollTop from './components/ScrollTop/ScrollTop';
 import { getProducts } from './redux/slices/productsSlice';
 
 const App = () => {
+  const { pathname } = useLocation();
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
 
   useEffect(() => {
     dispatch(getProducts());
--- client/src/pages/Menu/Menu.js
+++ client/src/pages/Menu/Menu.js
@@ -9,16 +9,17 @@ const MenuPage = () => {
   return (
     <>
       <RestaurantItem />
-      {products.slice(0, 6).map(({ price, imageUrl, name, rating, _id: id }) => (
-        <ProductCardItem
-          key={id}
-          price={price}
-          imageUrl={imageUrl}
-          name={name}
-          rating={rating}
-          id={id}
-        />
-      ))}
+      {/* для перевірки переходу на сторінку блюда при кліку на картку блюда */}
+      {/* {products.slice(0, 6).map(({ price, imageUrl, name, rating, _id: id }) => ( */}
+      {/*  <ProductCardItem */}
+      {/*    key={id} */}
+      {/*    price={price} */}
+      {/*    imageUrl={imageUrl} */}
+      {/*    name={name} */}
+      {/*    rating={rating} */}
+      {/*    id={id} */}
+      {/*  /> */}
+      {/* ))} */}
     </>
   );
 };
