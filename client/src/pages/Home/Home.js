413c7be0ba9ba905ad06195bdffd182984ccee11
Yurii Horodnii <y.horodniy@gmail.com>
2023-10-06 21:26:31 +0300

    create Features component

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Typography } from '@mui/material';
 import SectionGetStarted from '../../components/SectionGetStarted/SectionGetStarted';
+import Features from '../../components/Features/Features';
 
 const HomePage = () => {
   return (
-    <SectionGetStarted />
+    <>
+      <SectionGetStarted />
+      <Features />
+    </>
   );
 };
 
