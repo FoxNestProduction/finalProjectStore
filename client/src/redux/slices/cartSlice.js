c590bc0d495df456ed114b5352e6e2db64e1fe03
Yaroslav <129963434+yaroslavhroshev@users.noreply.github.com>
2023-10-07 14:44:35 +0300

    Change cartSlice and create constants.js

--- client/src/redux/slices/cartSlice.js
+++ client/src/redux/slices/cartSlice.js
@@ -1,10 +1,11 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { useSelector } from 'react-redux';
 import axios from 'axios';
+import { allProducts } from './productsSlice';
 
 const initialState = {
   cartItems: [],
-  setLoading: false,
+  isLoading: false,
   isCart: false,
 };
 
@@ -16,7 +17,7 @@ const cartSlice = createSlice({
   reducers: {
     addToCart(state, action) {
       // 1-ий варіант - коли нам в action.payload надходить повний об'єкт item
-      if (state.cartItems.length === 0) {
+      if (state.cartItems.length === 0 && action.payload !== null) {
         state.cartItems.push(action.payload);
       } else {
         const index = state.cartItems.findIndex((item) => item.id === action.payload.id);
@@ -26,60 +27,53 @@ const cartSlice = createSlice({
           state.cartItems[index].quantity += 1;
         }
       }
-      // 2-ий варіант, коли нам приходить суто id товара
-      // import { products } from './productsSlice'; - імпортуємо
-      // дуструктуроване значення всіх product зі slice зі всіма товарами
-      // або ж підписуємось на це значення за допомогою useSelector
-      //
-      // const cartItem = products.find(product => product.id === action.payload);
-      // if (cartItem !== null && cartItem !== undefined) {
-      //   if (state.cartItems.length === 0) {
-      //     state.cartItems.push(cartItem);
-      //   } else {
-      //     const index = state.cartItems.findIndex((item) => item.id === cartItem.id);
-      //     if (index === -1) {
-      //       state.cartItems.push(action.payload);
-      //     } else {
-      //       state.cartItems[index].quantity += 1;
-      //     }
-      //   }
-      // }
+      /** 2-ий варіант, коли нам приходить суто id товара потрібно звернутись
+       * до однієї з двох написаних нижче функцій:
+       * - deleteOrAddCartByItemId , або
+       * - deleteOrAddFromCartByItemIdWithValueFromState
+       * Потрібно буде протестувати, який варіант зручніше, той і використовувати
+       */
     },
     setCart(state, action) {
-      state.cartItems = action.payload; // в розробці логіка злиття тієї інформації яка вже є
+      state.cartItems = action.payload;
+
+      // в розробці логіка злиття тієї інформації яка вже є
       //  в localStorage, та яка приходить з сервера
     },
-    setLoading(state, action) {
-      state.loading = action.payload;
+    setIsLoading(state, action) {
+      state.isLoading = action.payload;
+    },
+    setIsCart(state, action) {
+      state.isCart = action.payload;
     },
     deleteFromCart(state, action) {
-      // 1-ий варіант - коли нам в action.payload надходить повний об'єкт item
-      // if (state.cartItems.length) {
-      //   const index = state.cartItems.findIndex((item) => item.id === action.payload.id);
+      // 1 - ий варіант - коли нам в action.payload надходить повний об'єкт item
+      if (state.cartItems.length) {
+        const index = state.cartItems.findIndex((item) => item.id === action.payload.id);
 
-      //   if (index !== -1) {
-      //     state.cartItems[index] = (
-      //       state.cartItems[index] === 1
-      //         ? state.cartItems.splice(index, 1)
-      //         : state.cartItems[index].quantity -= 1
-      //     );
-      //   }
-      // }
-      // 2-ий варіант - коли нам в action.payload надходить id об'єкта товара
-      // const products = useSelector((state) => state.products.products);
-      // такого значення ще не існує
-      // if (state.cartItems.length) {
-      //   const cartItem = products.find(product => product.id === action.payload);
-      // }
+        if (index !== -1) {
+          state.cartItems[index] = (
+            state.cartItems[index] === 1
+              ? state.cartItems.splice(index, 1)
+              : state.cartItems[index].quantity -= 1
+          );
+        }
+      }
+      /** 2-ий варіант, коли нам приходить суто id товара потрібно звернутись
+         * до однієї з двох написаних нижче функцій:
+         * - deleteOrAddCartByItemId , або
+         * - deleteOrAddFromCartByItemIdWithValueFromState
+         * Потрібно буде протестувати, який варіант зручніше, той і використовувати
+         */
     },
   },
 });
 
-export const { addToCart, setCart, setLoading, deleteFromCart } = cartSlice.actions;
+export const { addToCart, setCart, setIsLoading, deleteFromCart, setIsCart } = cartSlice.actions;
 
-const getCartItemsFromServer = () => async (dispatch) => {
+export const getCartItemsFromServer = () => async (dispatch) => {
   try {
-    dispatch(setLoading(true));
+    dispatch(setIsLoading(true));
     const userId = useSelector((state) => state.user.user.id);
     const { data } = await axios.get('http://localhost:4000/cart'); // якщо data undefined, то нічого не передаємо в кошик
     // а isCart(true or false). Код далі не виконується і дані на сторінці відмальовуються
@@ -88,11 +82,66 @@ const getCartItemsFromServer = () => async (dispatch) => {
 
     dispatch(setCart(cartContainer)); // якщо ми тут оновимо state то оновиться і localStorage
     //  тому тут потрібно написати логіку злиття даних з localStorage з даними з сервера.
-    dispatch(setLoading(false));
+    dispatch(setIsLoading(false));
   } catch (error) {
-    console.warn(error);
-    dispatch(setLoading(false));
+    console.warn('Error loading cart:', error);
+    dispatch(setIsLoading(false));
   }
+};
+
+/** 2 варіанти функції якщо ми додаємо або видаляємо товар в state cart через прокидування id:
+ * TODO: варіант A:
+ * ми робимо підписку на необхідний нам state всіх продуктів в самій функції.
+ * Знаходимо по id необхідний нам об'єкт, та прокидуємо його в один з reducer-ов
+ * за допомогою одного з ключів DELETE або ADD. Необхідний нам об'єкт падає
+ * в визначений reducer та додається до кошику, або ж видаляється з нього.
+ * Ця функція передається через dispatch. В неї прокидується id та key.
+ * dispatch(deleteOrAddCartByItemId(id, key))
+ */
+export const deleteOrAddCartByItemId = (id, key) => (dispatch) => {
+  const products = useSelector((state) => state.products.products);
+  if (products.length !== 0) {
+    const cartItem = products.find((product) => product.id === id);
+    if (cartItem !== null && cartItem !== undefined) {
+      if (key === 'ADD') {
+        dispatch(addToCart(cartItem));
+      }
+      if (key === 'DELETE') {
+        dispatch(deleteFromCart(cartItem));
+      }
+    }
+  }
+};
+
+/** 2 варіанти функції якщо ми додаємо або видаляємо товар в state cart через прокидування id:
+ * TODO: варіант B:
+ * всередині компонента товара ми робимо підписку через useSelector на цей самий об'єкт товара.
+ * Якщо точніше, то це не зовсім підписка, а пошук через функціональний селектор необхідних нам
+ * даних в останній версії store, завдяки функції useSelector. В самому компоненті потрібно буде
+ * прописати такий код:
+ * TODO: const fullItemObject = useSelector(deleteOrAddFromCartByItemIdWithValueFromState(id))
+ * в константу fullItemObject прийде повний об'єкт цього товара, і при кліку на кнопку
+ * "Додати до кошика" (в нашому випадку це знак +), або видалення з кошика
+ * ми передаємо в onClick такий код:
+ * додати до кошика
+ * TODO: onClick={dispatch(addToCart(fullItemObject))}
+ * або такий, якщо видалити з кошика
+ * TODO: onClick={dispatch(deleteFromCart(fullItemObject))}
+ * Попередньо імпортувавши dispatch та addToCart()/deleteFromCart()
+ */
+
+export const deleteOrAddFromCartByItemIdWithValueFromState = (id) => (state) => {
+  const products = allProducts(state);
+  console.log(products);
+  if (products.length !== 0) {
+    const cartItem = products.find((product) => product.id === id);
+    console.log(cartItem);
+    if (cartItem !== null && cartItem !== undefined) {
+      return cartItem;
+    }
+    return null;
+  }
+  return null;
 };
 
 export default cartSlice.reducer;
--- client/src/redux/slices/productsSlice.js
+++ client/src/redux/slices/productsSlice.js
@@ -26,4 +26,6 @@ export const getProducts = () => async (dispatch) => {
   }
 };
 
+export const allProducts = (state) => state.products.products;
+
 export default productsSlice.reducer;
