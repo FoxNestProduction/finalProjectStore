ca16513633ede586bf70c409617653df5060b845
Yaroslav <129963434+yaroslavhroshev@users.noreply.github.com>
2023-10-10 21:33:44 +0300

    CartPage is ready to use

--- client/src/components/Cart/Cart.js
+++ client/src/components/Cart/Cart.js
@@ -3,36 +3,93 @@ import {
   Typography,
   Container,
   Box,
+  Button,
 } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import {
+  mainBox,
+  title,
+  cartProductsContainer,
+  continueShoppingBtn,
+  priceAndContBtnWrapper,
+  textAndPriceWrapper,
+  deliveryTypography,
+  freeTypography,
+  priceWrapper,
+  price,
+  continueBtn,
+} from './styles';
 
 const Cart = () => {
+  const dispatch = useDispatch();
+  const cartProduct = useSelector((state) => state.cart.product);
+  const totalSum = 24.55;
+  const continueFn = () => {
+    console.log('Continue');
+  };
   return (
     <Container>
       <Box
-        sx={{
-          width: '100%',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: {
-            mobile: 'center',
-          },
-        }}
+        sx={mainBox}
       >
         <Typography
           variant="h2"
-          sx={{
-            fontWeight: {
-              mobile: 'fontWeightSemiBold',
-              desktop: 'fontWeightMedium',
-            },
-            alignSelf: {
-              mobile: 'center',
-              tablet: 'flex-start',
-            },
-          }}
+          sx={title}
         >
           Order
         </Typography>
+        <Box
+          sx={cartProductsContainer}
+        >
+          Тут знаходяться cartProduct
+        </Box>
+        <Button
+          LinkComponent={NavLink}
+          to="/menu"
+          variant="outlined"
+          sx={continueShoppingBtn}
+        >
+          Continue shopping
+        </Button>
+        <Box
+          sx={priceAndContBtnWrapper}
+        >
+          <Box
+            sx={textAndPriceWrapper}
+          >
+            <Typography
+              component="p"
+              sx={deliveryTypography}
+            >
+              Delivery
+            </Typography>
+            <Typography
+              component="p"
+              sx={freeTypography}
+            >
+              Free
+            </Typography>
+            <Box
+              sx={priceWrapper}
+            >
+              <Typography
+                component="p"
+                sx={price}
+              >
+                $
+                {totalSum}
+              </Typography>
+            </Box>
+          </Box>
+          <Button
+            variant="contained"
+            onClick={continueFn}
+            sx={continueBtn}
+          >
+            Continue
+          </Button>
+        </Box>
       </Box>
     </Container>
   );
---
+++ client/src/components/Cart/styles.js
@@ -0,0 +1,223 @@
+export const mainBox = {
+  width: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: {
+    mobile: 'center',
+  },
+};
+
+export const title = {
+  fontWeight: {
+    mobile: 'fontWeightSemiBold',
+    desktop: 'fontWeightMedium',
+  },
+  alignSelf: {
+    mobile: 'center',
+    tablet: 'flex-start',
+  },
+  color: 'text.primary',
+  mb: {
+    mobile: '22px',
+    tablet: '86px',
+    desktop: '26px',
+  },
+};
+
+export const cartProductsContainer = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  gap: {
+    mobile: '24px',
+    tablet: '30px',
+  },
+  mb: {
+    mobile: '24px',
+    tablet: '85px',
+    desktop: '40px',
+  },
+};
+
+export const continueShoppingBtn = {
+  mb: '24px',
+  height: {
+    mobile: '46px',
+    tablet: '56px',
+    lgTablet: '60px',
+    desktop: '62px',
+  },
+  fontSize: {
+    mobile: '18px',
+    tablet: '22px',
+    lgTablet: '24px',
+  },
+  fontWeight: 'fontWeightMedium',
+  transition: 'background-color 0.3s ease, box-shadow 0.3s ease, color 0.3s ease',
+  color: 'primary.main',
+  '&:hover': {
+    backgroundColor: 'primary.hover',
+    color: 'text.primaryLight',
+    boxShadow: `
+    0px 2px 4px -1px rgba(0,0,0,0.2),
+    0px 4px 5px 0px rgba(0,0,0,0.14),
+    0px 1px 10px 0px rgba(0,0,0,0.12)`,
+  },
+  '&:active': {
+    boxShadow: `
+    0px 5px 5px -3px rgba(0,0,0,0.2),
+    0px 8px 10px 1px rgba(0,0,0,0.14),
+    0px 3px 14px 2px rgba(0,0,0,0.12)`,
+    transform: 'translateY(1px)',
+    backgroundColor: 'common.white',
+    color: '#1C186C',
+    boxSizing: 'border-box',
+    border: '1px solid',
+    borderColor: 'primary.main',
+  },
+  alignSelf: {
+    lgTablet: 'flex-start',
+  },
+};
+
+export const priceAndContBtnWrapper = {
+  display: 'flex',
+  flexDirection: {
+    mobile: 'column',
+    lgTablet: 'row',
+  },
+  alignItems: 'center',
+  width: '100%',
+  mb: '200px',
+  gap: {
+    lgTablet: '32px',
+    desktop: '24px',
+  },
+};
+
+export const textAndPriceWrapper = {
+  display: 'flex',
+  justifyContent: {
+    mobile: 'space-between',
+    lgTablet: 'flex-start',
+  },
+  alignItems: {
+    mobile: 'center',
+    lgTablet: 'flex-start',
+  },
+  width: {
+    mobile: '100%',
+    lgTablet: '79%',
+  },
+  mb: {
+    mobile: '24px',
+    lgTablet: '0px',
+  },
+  gap: {
+    lgTablet: '7%',
+    desktop: '80px',
+  },
+};
+
+export const deliveryTypography = {
+  fontSize: {
+    mobile: '18px',
+    tablet: '26px',
+    lgTablet: '30px',
+    // tablet: '40px',
+    desktop: '38px',
+    // desktop: '48px',
+  },
+  fontWeight: {
+    mobile: 'fontWeightSemiBold',
+    desktop: 'fontWeightMedium',
+  },
+  color: 'text.primary',
+  fontFamily: 'fontPoppins',
+  flex: {
+    lgTablet: '1 1 15%',
+    desktop: '0',
+  },
+};
+
+export const freeTypography = {
+  fontWeight: 'fontWeightSemiBold',
+  fontFamily: 'fontPoppins',
+  color: 'secondary.main',
+  flex: {
+    lgTablet: '1 1 20%',
+  },
+  fontSize: {
+    mobile: '16px',
+    tablet: '22px',
+    lgTablet: '24px',
+  },
+  lineHeight: '1.5em',
+};
+
+export const priceWrapper = {
+  width: 'max-content',
+  border: {
+    tablet: '1px solid',
+  },
+  borderColor: {
+    tablet: 'text.primary',
+  },
+  borderRadius: '16px',
+};
+
+export const price = {
+  color: 'text.primary',
+  fontFamily: 'fontPoppins',
+  fontWeight: {
+    mobile: 'fontWeightBold',
+    desktop: 'fontWeightSemiBold',
+  },
+  fontSize: {
+    mobile: '18px',
+    tablet: '22px',
+    lgTablet: '24px',
+  },
+  textAlign: 'center',
+  p: {
+    tablet: '11px 28px',
+    lgTablet: '12px 32px',
+    desktop: '13px 44px',
+  },
+};
+
+export const continueBtn = {
+  width: {
+    mobile: '100%',
+    tablet: '170px',
+    desktop: '172px',
+  },
+  height: {
+    mobile: '46px',
+    tablet: '56px',
+    lgTablet: '60px',
+    desktop: '62px',
+  },
+  fontSize: {
+    mobile: '18px',
+    tablet: '22px',
+    lgTablet: '24px',
+    transition: 'background-color 0.3s ease, box-shadow 0.3s ease, color 0.3s ease',
+  },
+  color: 'text.primaryLight',
+  fontWeight: {
+    mobile: 'fontWeightBold',
+    desktop: 'fontWeightMedium',
+  },
+  '&:hover': {
+    backgroundColor: 'primary.hover',
+  },
+  '&:active': {
+    transform: 'translateY(1px)',
+    backgroundColor: 'common.white',
+    color: '#1C186C',
+    boxSizing: 'border-box',
+    border: '1px solid',
+    borderColor: 'primary.main',
+  },
+};
