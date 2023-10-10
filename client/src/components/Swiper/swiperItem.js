5e9fba3a228055f668d0870dc5d4b0da2f9d3e46
romadzhavadov <r.dzhavadov12@gmail.com>
2023-10-11 00:42:07 +0300

    work with swiper

--- client/src/components/Swiper/swiperItem.js
+++ client/src/components/Swiper/swiperItem.js
@@ -1,80 +1,116 @@
 import React, { useState, useEffect } from 'react';
-// Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { EffectCube, Pagination } from 'swiper/modules';
 
-// Import Swiper styles
 import 'swiper/scss';
 import 'swiper/css/effect-cube';
 import 'swiper/css/pagination';
 
-// import './.App/scss';
-
 import styles from './Swiper.module.scss';
-// import required modules
 
 const SwiperItem = () => {
   const [swiperInstance, setSwiperInstance] = useState(null);
+  const [activeSlideIndex, setActiveSlideIndex] = useState(0);
+  console.log(swiperInstance);
+
   useEffect(() => {
-    // Функція для оновлення активного слайду
     const updateActiveSlide = () => {
       if (swiperInstance) {
-        console.log(swiperInstance);
         const currentIndex = swiperInstance.activeIndex;
-        console.log(currentIndex);
         const slidesLength = swiperInstance.slides.length;
         const nextIndex = (currentIndex + 1) % slidesLength;
-        console.log(nextIndex);
-        swiperInstance.slideTo(nextIndex);
+        setActiveSlideIndex(nextIndex);
+        swiperInstance.slideToLoop(nextIndex);
       }
     };
 
-    // Встановлюємо інтервал для автоматичної зміни слайдів
-    // const interval = setInterval(updateActiveSlide, 3000);
+    const interval = setInterval(updateActiveSlide, 3000);
 
-    // При зміні активного слайду, очищаємо інтервал
     return () => {
-      // clearInterval(interval);
+      clearInterval(interval);
     };
   }, [swiperInstance]);
 
-  return (
-    <Swiper
-      className={styles.wrap}
-      effect="cube"
-      grabCursor
-      cubeEffect={{
-        shadow: true,
-        slideShadows: true,
-        shadowOffset: 20,
-        shadowScale: 0.94,
-      }}
-      modules={[EffectCube, Pagination]}
-      // className="mySwiper"
-      onSwiper={(swiper) => setSwiperInstance(swiper)}
-      pagination={{ clickable: true, el: '.custom-pagination' }}
-    >
-      <SwiperSlide>
-        <img src="https://swiperjs.com/demos/images/nature-1.jpg" alt="Slide 1" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src="https://swiperjs.com/demos/images/nature-2.jpg" alt="Slide 2" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src="https://swiperjs.com/demos/images/nature-3.jpg" alt="Slide 3" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src="https://swiperjs.com/demos/images/nature-4.jpg" alt="Slide 4" />
-      </SwiperSlide>
+  const handleBulletClick = (index) => {
+    console.log(index);
+    setActiveSlideIndex(index);
+    swiperInstance.slideTo(index);
+  };
 
+  const paginationData = [
+    { index: 0 },
+    { index: 1 },
+    { index: 2 },
+    // { index: 3 },
+  ];
+
+  return (
+    <div className={styles.swiperContainer}>
+      <Swiper
+        className={styles.wrap}
+        effect="cube"
+        grabCursor
+        cubeEffect={{
+          shadow: true,
+          slideShadows: true,
+          shadowOffset: 20,
+          shadowScale: 0.94,
+        }}
+        modules={[EffectCube, Pagination]}
+        pagination={{ clickable: true, el: '.customPaginationItem' }}
+        onSwiper={(swiper) => setSwiperInstance(swiper)}
+        onSlideChange={(swiper) => {
+          setActiveSlideIndex(swiper.activeIndex);
+        }}
+      >
+        <SwiperSlide>
+          <img src="./img/Banner.jpg" alt="Slide 1" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="./img/Banner2.jpg" alt="Slide 2" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="./img/Banner3.jpg" alt="Slide 3" />
+        </SwiperSlide>
+        {/* <SwiperSlide>
+          <img src="./img/Banner.jpg" alt="Slide 4" />
+        </SwiperSlide> */}
+      </Swiper>
       <div className={styles.customPagination}>
-        <div className={styles.customPaginationItem}>1</div>
-        <div className="custom-pagination-item">2</div>
-        <div className="custom-pagination-item">3</div>
-        <div className="custom-pagination-item">4</div>
+        {paginationData.map((item) => (
+          // eslint-disable-next-line jsx-a11y/control-has-associated-label
+          <div
+            key={item.index}
+            className={`${styles.customPaginationItem} ${
+              item.index === activeSlideIndex ? styles.active : ''
+            }`}
+            onClick={() => handleBulletClick(item.index)}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                handleBulletClick(item.index);
+              }
+            }}
+            role="button"
+            tabIndex={0}
+          />
+        ))}
       </div>
-    </Swiper>
+
+    </div>
   );
 };
 
 export default SwiperItem;
+
+// <SwiperSlide>
+// <img src="https://swiperjs.com/demos/images/nature-1.jpg" alt="Slide 1" />
+// </SwiperSlide>
+// <SwiperSlide>
+// <img src="https://swiperjs.com/demos/images/nature-2.jpg" alt="Slide 2" />
+// </SwiperSlide>
+// <SwiperSlide>
+// <img src="https://swiperjs.com/demos/images/nature-3.jpg" alt="Slide 3" />
+// </SwiperSlide>
+// <SwiperSlide>
+// <img src="https://swiperjs.com/demos/images/nature-4.jpg" alt="Slide 4" />
+// </SwiperSlide>
