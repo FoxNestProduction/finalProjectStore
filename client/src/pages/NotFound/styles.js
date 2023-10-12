9bb755178020b360e83e305393630591277f0929
Ihor Kacher <ihorkacher@gmail.com>
2023-10-12 02:24:00 +0200

    feature: add 404 page

---
+++ client/src/pages/NotFound/styles.js
@@ -0,0 +1,97 @@
+export const stylesWrapper = {
+  position: 'relative',
+  display: 'flex',
+  flexDirection: 'column',
+  height: '80vh',
+  alignItems: 'center',
+  justifyContent: 'center',
+  textAlign: 'center',
+  transition: '.3s',
+};
+
+export const stylesStatusWrapper = {
+  display: 'flex',
+  justifyContent: 'space-around',
+  width: '100%',
+};
+export const stylesStatus = {
+  fontSize: {
+    mobile: '100px',
+    tablet: '140px',
+    desktop: '180px',
+  },
+  color: 'primary.main',
+  fontWeight: 'fontWeightSemiBold',
+  fontFamily: 'fontPoppins',
+  lineHeight: {
+    mobile: '64px',
+    tablet: '130px',
+    desktop: '150px',
+  },
+  marginBottom: {
+    mobile: '40px',
+    tablet: '60px',
+  },
+};
+
+export const stylesDescription = {
+  fontSize: {
+    mobile: '16px',
+    tablet: '20px',
+    desktop: '24px',
+  },
+  color: 'text.primary',
+  fontWeight: 'fontWeightSemiBold',
+  fontFamily: 'fontPoppins',
+  lineHeight: {
+    mobile: '24px',
+    tablet: '28px',
+    desktop: '32px',
+  },
+  marginBottom: {
+    mobile: '47px',
+    tablet: '60px',
+  },
+};
+
+export const stylesLink = {
+  backgroundColor: 'primary.main',
+  '&:hover': {
+    backgroundColor: 'primary.hover',
+  },
+  height: '60px',
+  padding: {
+    mobile: '17px 55px',
+    tablet: '17px 80px',
+    desktop: '17px 100px',
+  },
+  fontSize: {
+    mobile: '16px',
+    tablet: '20px',
+    desktop: '24px',
+  },
+  color: 'text.primaryLight',
+  fontWeight: 500,
+  fontFamily: 'fontPoppins',
+  lineHeight: {
+    mobile: '16px',
+    tablet: '20px',
+    desktop: '24px',
+  },
+};
+
+export const stylesRightArrow = {
+  position: 'relative',
+  top: '-10%',
+  width: '122px',
+  display: {
+    mobile: 'none',
+    tablet: 'block',
+  },
+};
+
+export const stylesLeftArrow = {
+  ...stylesRightArrow,
+  transform: 'scaleX(-1)',
+
+};
