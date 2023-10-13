192b6655abc5092fdf36187a5b22ee716e5037cb
romadzhavadov <r.dzhavadov12@gmail.com>
2023-10-13 16:01:11 +0300

    finish swiper component

--- client/src/components/SwiperBanner/SwiperBanner.js
+++ client/src/components/SwiperBanner/SwiperBanner.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { EffectCube, Pagination } from 'swiper/modules';
+import { Autoplay, EffectCube, Pagination } from 'swiper/modules';
 import classNames from 'classnames';
 
 import 'swiper/scss';
@@ -12,65 +12,21 @@ import styles from './SwiperBanner.module.scss';
 const SwiperBanner = () => {
   const [swiperInstance, setSwiperInstance] = useState(null);
   const [activeSlideIndex, setActiveSlideIndex] = useState(0);
-  console.log(swiperInstance);
 
-  useEffect(() => {
-    const updateActiveSlide = () => {
-      if (swiperInstance) {
-        const currentIndex = swiperInstance.activeIndex;
-        const slidesLength = swiperInstance.slides.length;
-        const nextIndex = (currentIndex + 1) % slidesLength;
-        setActiveSlideIndex(nextIndex);
-        swiperInstance.slideToLoop(nextIndex);
-        // const newIndex = (currentIndex + 1);
-        // if (newIndex === slidesLength) {
-        //   setActiveSlideIndex(0);
-        //   swiperInstance.slideToLoop(0);
-        // } else {
-        //   const nextIndex = (currentIndex + 1);
-        //   setActiveSlideIndex(nextIndex);
-        //   swiperInstance.slideToLoop(nextIndex);
-        // }
-      }
-    };
+  const data = [
+    { url: './img/Banner.jpg', index: 0 },
+    { url: './img/Banner2.jpg', index: 1 },
+    { url: './img/Banner3.jpg', index: 2 },
+  ];
 
-    const interval = setInterval(updateActiveSlide, 3000);
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, [swiperInstance]);
-
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     if (swiperInstance) {
-  //       setActiveSlideIndex(0);
-  //       swiperInstance.slideTo(0);
-  //     }
-  //   }, 3000);
-
-  //   return () => {
-  //     clearInterval(interval);
-  //   };
-  // }, [swiperInstance]);
-
-  const handleBulletClick = (index) => {
-    console.log(index);
-    setActiveSlideIndex(index);
-    swiperInstance.slideTo(index);
+  const handleSlideChange = (swiper) => {
+    setActiveSlideIndex(swiper.realIndex);
   };
 
-  const paginationData = [
-    { index: 0 },
-    { index: 1 },
-    { index: 2 },
-  ];
-
-  const urlData = [
-    { url: './img/Banner.jpg' },
-    { url: './img/Banner2.jpg' },
-    { url: './img/Banner3.jpg' },
-  ];
+  const handlePaginationClick = (index) => {
+    setActiveSlideIndex(index);
+    swiperInstance.slideToLoop(index);
+  };
 
   return (
     <>
@@ -84,64 +40,36 @@ const SwiperBanner = () => {
           shadowOffset: 20,
           shadowScale: 0.94,
         }}
-        modules={[EffectCube, Pagination]}
+        loop
+        modules={[Autoplay, EffectCube, Pagination]}
         pagination={{ clickable: true, el: '.customPaginationItem' }}
         onSwiper={(swiper) => setSwiperInstance(swiper)}
-        onSlideChange={(swiper) => {
-          setActiveSlideIndex(swiper.activeIndex);
-        }}
+        onSlideChange={handleSlideChange}
         speed={700}
-        // loop
-        // onReachEnd={(swiper) => {
-        //   setActiveSlideIndex(swiper.activeIndex);
-        //   if (swiper.activeIndex === swiper.slides.length - 1) {
-        //     swiper.slideTo(0);
-        //   }
-        // }}
+        autoplay={{
+          delay: 4000,
+          disableOnInteraction: false,
+        }}
       >
-        {urlData.map((item) => (
-          <SwiperSlide className={styles.swiperSlide}>
-            <div className={styles.swiperSlideItem}>
-              <img src={item.url} alt="Slide" />
+        {data.map((item) => (
+          <SwiperSlide className={styles.swiperSlide} key={item.index}>
+            <div id={item.index} className={styles.swiperSlideItem}>
+              <img src={item.url} alt={`Slide${item.index}`} />
             </div>
           </SwiperSlide>
         ))}
-
-        {/* <SwiperSlide className={styles.swiperSlide}>
-          <div className={styles.swiperSlideItem}>
-            <img src="./img/Banner.jpg" alt="Slide 1" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className={styles.swiperSlide}>
-          <div className={styles.swiperSlideItem}>
-            <img src="./img/Banner2.jpg" alt="Slide 2" />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className={styles.swiperSlide}>
-          <div className={styles.swiperSlideItem}>
-            <img src="./img/Banner3.jpg" alt="Slide 3" />
-          </div>
-        </SwiperSlide> */}
       </Swiper>
       <div className={styles.customPagination}>
-        {paginationData.map((item) => (
-          // eslint-disable-next-line jsx-a11y/control-has-associated-label
+        {data.map((item) => (
+          // eslint-disable-next-line max-len
+          // eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions
           <div
+            id={item.index}
             key={item.index}
-            className={
-              classNames(
-                styles.customPaginationItem,
-                { [styles.active]: item.index === activeSlideIndex },
-              )
-            }
-            onClick={() => handleBulletClick(item.index)}
-            onKeyDown={(e) => {
-              if (e.key === 'Enter') {
-                handleBulletClick(item.index);
-              }
-            }}
-            role="button"
-            tabIndex={0}
+            className={classNames(styles.customPaginationItem, {
+              [styles.active]: item.index === activeSlideIndex,
+            })}
+            onClick={() => handlePaginationClick(item.index)}
           />
         ))}
       </div>
