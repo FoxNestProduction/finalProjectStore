12e39389b64982935bb33b4a0a61e3335a30f8a1
Yurii Horodnii <y.horodniy@gmail.com>
2023-10-15 08:57:31 +0300

    done ReviewPage

--- client/src/pages/Reviews/Reviews.js
+++ client/src/pages/Reviews/Reviews.js
@@ -1,17 +1,20 @@
-import React from 'react';
+/* eslint-disable no-underscore-dangle */
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Typography, Stack, Button } from '@mui/material';
+import { Typography, Stack, Button, Box, Container, useMediaQuery, useTheme } from '@mui/material';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import ReviewItem from '../../components/ReviewItem/ReviewItem';
 import Modal from '../../components/Modal/Modal';
 import NewReview from '../../components/NewReview/NewReview';
 import { openModal, setTitle, setContent, setButtonAgree, addButtonBox, closeModal } from '../../redux/slices/modalSlice';
 import { addNewReview, setNewReview } from '../../redux/slices/reviewsSlice';
+import { TitleBtn, commentItem, commentList, container, flexCenter, titleContainer } from './styles';
 
 const ReviewsPage = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
   const dispatch = useDispatch();
   const reviews = useSelector((state) => state.reviews.reviews);
-  const newReview = useSelector((state) => state.reviews.newReviews);
-  const token = useSelector((state) => state.authorization.token);
+  const isLgTablet = useMediaQuery('(min-width: 690px)');
 
   const handleSendFeedback = () => {
     dispatch(addNewReview());
@@ -33,18 +36,34 @@ const ReviewsPage = () => {
     dispatch(addButtonBox(true));
   };
 
+  const incrementIndex = () => {
+    setCurrentIndex((prevIndex) => prevIndex + 1);
+  };
+
+  useEffect(() => {
+    const intervalId = setInterval(incrementIndex, 400);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
   return (
-    <>
-      <Button variant="standard" onClick={handleOpenModalReview}>
-        Open modal with review
-      </Button>
-      <Typography variant="h2">Customer Say</Typography>
-      <Stack>
-        {/* eslint-disable-next-line no-underscore-dangle */}
-        {reviews.map((item) => <ReviewItem key={item.comment} review={item} />)}
-      </Stack>
-      <Modal />
-    </>
+    <Container component="section" sx={{ ...flexCenter, ...container }}>
+      <Box sx={titleContainer}>
+        <Typography variant="h2" sx={{ justifySelf: 'center' }}>Customer Say</Typography>
+        <Button variant="standard" sx={TitleBtn} onClick={handleOpenModalReview}>
+          {isLgTablet && <Typography mr={1}>Create your oun review</Typography>}
+          <AddCircleOutlineIcon />
+        </Button>
+      </Box>
+      <Box sx={commentList}>
+        {reviews.slice(0, currentIndex).map((item) => (
+          <Box sx={commentItem}>
+            <ReviewItem key={item._id} review={item} />
+          </Box>
+        ))}
+      </Box>
+    </Container>
   );
 };
 
