386be79518b33350c5797fffb37a82055747e78e
Stephan <s.dumenko@icloud.com>
2023-10-18 23:12:52 +0200

    add merge

--- server/config/getConfigs.js
+++
@@ -1,6 +0,0 @@
-const GlobalConfig = require("../models/GlobalConfig");
-
-module.exports = async () => {
-  const configs = await GlobalConfig.findOne({ customId: "eatly-global-configs" });
-  return configs;
-};
