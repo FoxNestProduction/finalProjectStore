ec25696a4ce774362cf8ac05c26be90399aee9b9
Yurii Horodnii <y.horodniy@gmail.com>
2023-10-30 13:23:14 +0200

    change skeleton (home)

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { shallowEqual, useSelector } from 'react-redux';
+import { Box, Container } from '@mui/material';
 import SectionGetStarted from '../../components/SectionGetStarted/SectionGetStarted';
 import ListItems from '../../components/ListItems/ListItem';
 import RestaurantItem from '../../components/RestaurantItem/RestaurantItem';
@@ -11,33 +12,54 @@ import ProductCardItem from '../../components/ProductCardItem/ProductCardItem';
 import { instance } from '../../API/instance';
 import SwiperBanner from '../../components/SwiperBanner/SwiperBanner';
 import Skeleton from '../../components/Skeleton/Skeleton';
+import { gridStylesContainer } from '../../components/ListItems/styles';
 
 const HomePage = () => {
-  const topProducts = useSelector((state) => state.products.topProducts, shallowEqual);
   const topPartners = useSelector((state) => state.partners.topPartners, shallowEqual);
+  const loadingPartners = useSelector((state) => state.partners.loading);
+  const topProducts = useSelector((state) => state.products.topProducts, shallowEqual);
+  const loadingProducts = useSelector((state) => state.products.loading);
 
   return (
     <>
       <SectionGetStarted />
       <Features />
       <MobileApp />
-      {topPartners.length > 0 && (
-      <ListItems
-        title="Our Top Restaurants"
-        items={topPartners}
-        itemComponent={RestaurantItem}
-        actions={<ListItemAction type="partners" />}
-        type="partners"
-      />
-      ) }
-      {topProducts.length > 0 && (
+      {loadingPartners ? (
+        <Container sx={{ mb: 13 }}>
+          <Box sx={gridStylesContainer}>
+            <Skeleton skeletonType="restaurant" />
+            <Skeleton skeletonType="restaurant" />
+            <Skeleton skeletonType="restaurant" />
+          </Box>
+        </Container>
+      ) : (topPartners.length > 0 && (
+        <ListItems
+          title="Our Top Restaurants"
+          items={topPartners}
+          itemComponent={RestaurantItem}
+          actions={<ListItemAction type="partners" />}
+          type="partners"
+        />
+      )) }
+      {loadingProducts ? (
+        <Container sx={{ mb: 13 }}>
+          <Box sx={gridStylesContainer}>
+            <Skeleton skeletonType="product" />
+            <Skeleton skeletonType="product" />
+            <Skeleton skeletonType="product" />
+            <Skeleton skeletonType="product" />
+            <Skeleton skeletonType="product" />
+          </Box>
+        </Container>
+      ) : (topProducts.length > 0 && (
       <ListItems
         title="Our Top Dishes"
         items={topProducts}
         itemComponent={ProductCardItem}
         actions={<ListItemAction />}
       />
-      ) }
+      )) }
       <SwiperReview />
     </>
   );
