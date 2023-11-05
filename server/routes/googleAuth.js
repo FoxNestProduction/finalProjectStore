30da69c0a09567376df96bfac8c0f45f3b600ade
Yurii Horodnii <y.horodniy@gmail.com>
2023-11-05 15:53:26 +0200

    add server files

---
+++ server/routes/googleAuth.js
@@ -0,0 +1,54 @@
+const router = require('express').Router();
+const passport = require('passport');
+
+router.get(
+    '/login/success',
+    (req, res) => {
+        console.log(req.query);
+        if(req.user) {
+            res.status(200).json({
+                error: false,
+                message: 'Successfully Loged In',
+                user: req.user,
+            });
+        } else {
+            res.status(403).json({
+                error: true,
+                message: 'Not Authorized'
+            });
+        };
+    }
+);
+
+router.get(
+    '/login/failed',
+    (req, res) => {
+        res.status(401).json({
+            error: true,
+            message:'Log in Failure',
+        })
+    }
+);
+
+router.get(
+    '/google/callback',
+    passport.authenticate('google', {
+        successRedirect: `${process.env.SERVER_URL}/api/auth/login/success`,
+        failureRedirect: '/login/failed',
+    })
+);
+
+router.get(
+    '/google',
+    passport.authenticate('google', ['profile', 'email'])
+)
+
+router.get(
+    '/logout',
+    (req, res) => {
+        req.logOut();
+        res.redirect(process.env.CLIENT_URL);
+    }
+);
+
+module.exports = router;
