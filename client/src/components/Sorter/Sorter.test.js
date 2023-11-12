54f8187656a92ea0ddca6ddfe6c8f166307fccfb
dmitriykud <0955319181d@gmail.com>
2023-11-12 12:35:06 +0200

    add test in logo, footer, sorter

--- client/src/components/Sorter/Sorter.test.js
+++ client/src/components/Sorter/Sorter.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent, render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import Sorter from './Sorter';
 import store from '../../redux/store';
