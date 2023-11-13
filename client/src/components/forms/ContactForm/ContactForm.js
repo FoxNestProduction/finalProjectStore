37e0fcc1504a596fb5800cb523175d9a82d81af0
Anna Shtendera <ann.shtendera@gmail.com>
2023-11-13 22:47:41 +0200

    add message to contact form

--- client/src/components/forms/ContactForm/ContactForm.js
+++ client/src/components/forms/ContactForm/ContactForm.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Formik, Form } from 'formik';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import { CircularProgress } from '@mui/material';
 import validationSchema from './validationSchema';
 import {
   mainTitle,
@@ -15,8 +16,13 @@ import Input from '../../inputs/Input/Input';
 import Textarea from '../../inputs/Textarea/Textarea';
 import OneLoopArrowSvg from '../../../assets/svgComponents/OneLoopArrowSvg';
 import { instance } from '../../../API/instance';
+import useGetAPI from '../../../customHooks/useGetAPI';
 
 const ContactForm = () => {
+  const [messageReceived, setMessageReceived] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
   const initialValues = {
     name: '',
     email: '',
@@ -26,12 +32,27 @@ const ContactForm = () => {
   const handleSubmit = async (values, actions) => {
     console.log(values);
     try {
+      setLoading(true);
+      setError(false);
+      setMessageReceived(false);
       const response = await instance.post('/support', values);
       console.log(response);
+      if (response.status === 200) {
+        setMessageReceived(true);
+      }
+      actions.resetForm();
     } catch (err) {
       console.error('Error sending customer support request: ', err);
+      setMessageReceived(false);
+      setError(true);
+    } finally {
+      setLoading(false);
     }
-    actions.resetForm();
+  };
+
+  const handleFieldChange = (e, handleChange) => {
+    handleChange(e);
+    setMessageReceived(false);
   };
 
   return (
@@ -44,7 +65,7 @@ const ContactForm = () => {
         onSubmit={handleSubmit}
         validationSchema={validationSchema}
       >
-        {({ isValid }) => (
+        {({ isValid, handleChange }) => (
           <Form>
             <Box sx={inputsWrapper}>
               <Input
@@ -54,6 +75,9 @@ const ContactForm = () => {
                 placeholder="Enter your name"
                 label="Name"
                 bgColor="#FFF"
+                onChange={(e) => {
+                  handleFieldChange(e, handleChange);
+                }}
               />
               <Input
                 type="email"
@@ -62,18 +86,41 @@ const ContactForm = () => {
                 placeholder="Enter your e-mail"
                 label="E-mail"
                 bgColor="#FFF"
+                onChange={(e) => {
+                  handleFieldChange(e, handleChange);
+                }}
               />
-              <Textarea name="message" placeholder="Enter the problem or query..." />
+              <Textarea
+                name="message"
+                placeholder="Enter the problem or query..."
+                onChange={(e) => {
+                  handleFieldChange(e, handleChange);
+                }}
+              />
             </Box>
+            {messageReceived && (
+            <Typography variant="body1" component="p" sx={{ color: 'primary.main', mb: '10px', pl: '5px' }}>
+              Thank you for reaching out!
+              We&apos;ve received your inquiry and will get back to you as soon as possible.
+            </Typography>
+            )}
+            {error && (
+              <Typography variant="body1" component="p" sx={{ color: 'text.error', mb: '10px', pl: '5px' }}>
+                Oops...Something went wrong.
+                Try to send your inquiry again.
+              </Typography>
+            )}
             <Box sx={{ textAlign: 'center' }}>
               <Button
                 disableRipple
                 variant="contained"
                 sx={sendBtn}
                 type="submit"
-                disabled={!isValid}
+                disabled={!isValid || loading}
               >
-                Send Now
+                {!loading
+                  ? 'Send Now'
+                  : <CircularProgress color="primary" />}
               </Button>
             </Box>
           </Form>
