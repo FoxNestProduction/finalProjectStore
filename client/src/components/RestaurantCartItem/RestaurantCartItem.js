0aa1af5a61433bc6e0f6153e8534ea434a038c55
Yaroslav <129963434+yaroslavhroshev@users.noreply.github.com>
2023-11-15 21:17:52 +0200

    Create RestaurantCartItem

---
+++ client/src/components/RestaurantCartItem/RestaurantCartItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box } from '@mui/material';
+
+const RestaurantCartItem = () => {
+  return (
+    <Box>
+      Render ProductCartItems
+    </Box>
+  );
+};
+
+export default RestaurantCartItem;
