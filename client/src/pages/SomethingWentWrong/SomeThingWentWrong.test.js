fee4ad4da3e456bf5a858c117e682373c53405d7
Kovtun Oksana <pavoks962@gmail.com>
2023-11-20 11:47:56 +0200

    tested pages AboutUs, Checkout, Contact, NotFound, OrderConfirmation, Product, somethingWentWrong

---
+++ client/src/pages/SomethingWentWrong/SomeThingWentWrong.test.js
@@ -0,0 +1,31 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import { MemoryRouter, useNavigate, useLocation } from 'react-router-dom';
+import SomethingWentWrong from './SomethingWentWrong';
+
+jest.mock('react-router', () => ({
+  ...jest.requireActual('react-router'),
+  useLocation: jest.fn(),
+  useNavigate: jest.fn(),
+}));
+
+describe('SomethingWentWrong', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('should render SomethingWentWrong', () => {
+    const navigateMock = jest.fn();
+    useNavigate.mockReturnValue(navigateMock);
+
+    const { asFragment } = render(
+      <MemoryRouter>
+        <SomethingWentWrong />
+      </MemoryRouter>
+    );
+    expect(asFragment()).toMatchSnapshot();
+
+    const reloadButton = screen.getByText('Reload');
+
+    expect(reloadButton).toBeInTheDocument();
+  });
+});
