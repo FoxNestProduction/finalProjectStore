4cba39d62b6ca2b3c7f0316b09e7fb15036b9a02
Yurii Horodnii <y.horodniy@gmail.com>
2023-11-22 01:26:59 +0200

    add Skeleton test

--- client/src/components/forms/RegisterForm/validationSchema.js
+++ client/src/components/forms/RegisterForm/validationSchema.js
@@ -13,8 +13,10 @@ const validationSchema = object({
   password: string()
     .required('Password is required')
     .min(8, 'Password is too short - should be 8 chars minimum')
-    .matches('^[A-Z][a-z]+$', 'Password must contain only Latin letters')
-    .matches('[!@#$%^&*(),.?":{}|<>]'),
+    .matches('(.*[a-z].*)', 'Password must contain only Latin letters and special symbol')
+    .matches('(.*[A-Z].*)', 'Password must contain only Latin letters and special symbol')
+    .matches('(.*\\d.*)', 'Password must contain only Latin letters and special symbol')
+    .matches('[!@#$%^&*(),.?":{}|<>]', 'Password must contain only Latin letters and special symbol'),
 });
 
 export default validationSchema;
