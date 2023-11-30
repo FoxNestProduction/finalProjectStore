eece2174b3992acfee40122da26d8c6890fdf414
romadzhavadov <r.dzhavadov12@gmail.com>
2023-11-30 17:48:35 +0200

    add url and json to i18n

--- client/src/i18n.js
+++ client/src/i18n.js
@@ -1,10 +1,10 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import i18nBackend from 'i18next-http-backend';
-// import LanguageDetector from 'i18next-browser-languagedetector';
-// const getCurrentHost = import.meta.env.MODE === 'development'
-//   ? 'http://localhost:5173'
-//   : 'LINK TO PROD';
+
+const getCurrentHost = process.env.NODE_ENV === 'development'
+  ? process.env.REACT_APP_URL
+  : process.env.REACT_APP_URL_PROD;
 
 i18n
   .use(i18nBackend)
@@ -16,31 +16,8 @@ i18n
       escapeValue: false,
     },
     backend: {
-      // loadPath: `${getCurrentHost}/i18n/{{lng}}.json`,
-      loadPath: 'http://localhost:3000/i18n/{{lng}}.json',
+      loadPath: `${getCurrentHost}/i18n/{{lng}}.json`,
     },
-    // resources: {
-    //   en: {
-    //     translation: {
-    //       menu: 'Menu',
-    //       restaurants: 'Restaurants',
-    //       reviews: 'Reviews',
-    //       contact: 'Contact',
-    //       aboutUs: 'About Us',
-    //       rights: '© 2023 EATLY All Rights Reserved.',
-    //     },
-    //   },
-    //   ua: {
-    //     translation: {
-    //       menu: 'Меню',
-    //       restaurants: 'Ресторани',
-    //       reviews: 'Відгуки',
-    //       contact: 'Контакт',
-    //       aboutUs: 'Про нас',
-    //       rights: '© 2023 EATLY Всі права захищені.',
-    //     },
-    //   },
-    // },
   });
 
 export default i18n;
