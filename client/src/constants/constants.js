845a9fd6060545c36c0c1acc022f0f1e73a340ec
Kovtun Oksana <pavoks962@gmail.com>
2023-12-19 16:48:31 +0200

    added navigator.language

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -3,7 +3,7 @@ export const DELETE = 'DELETE';
 export const CHECKOUT_SS_KEY = 'checkoutInfo';
 
 export const LANGUAGES = [
-  { label: 'UA', code: 'ua' },
+  { label: 'UA', code: 'uk' },
   { label: 'EN', code: 'en' },
   { label: 'PL', code: 'pl' },
 ];
--- client/src/i18n.js
+++ client/src/i18n.js
@@ -6,12 +6,16 @@ const getCurrentHost = process.env.NODE_ENV === 'development'
   ? process.env.REACT_APP_URL
   : process.env.REACT_APP_URL_PROD;
 
+const userLanguage = navigator.language.split('-')[0];
+console.log(navigator.language);
+
 i18n
   .use(i18nBackend)
   .use(initReactI18next)
   .init({
     fallbackLng: 'en', // мова, яка буде завантажена, якщо переклади, які шукає користувач, будуть недоступні
-    lng: 'en', // мова за замовчуванням
+    // lng: 'en', // мова за замовчуванням
+    lng: userLanguage, // за замовчуванням мова браузера
     interpolation: {
       escapeValue: false, // використовується для екранування значень і уникнення атак XSS
     },
