f5a447973c4ce7cc2bfe5374aa2b8a2b063ab5ae
Kovtun Oksana <pavoks962@gmail.com>
2023-12-19 17:05:07 +0200

    added comment for eslint about navigator is global

--- client/src/i18n.js
+++ client/src/i18n.js
@@ -5,25 +5,24 @@ import i18nBackend from 'i18next-http-backend';
 const getCurrentHost = process.env.NODE_ENV === 'development'
   ? process.env.REACT_APP_URL
   : process.env.REACT_APP_URL_PROD;
+  
+/* global navigator */
+const userLanguage = navigator.language.split('-')[0];
+console.log(navigator.language);
 
-if (typeof window !== 'undefined') {
-  const userLanguage = navigator.language.split('-')[0];
-  console.log(navigator.language);
-
-  i18n
-    .use(i18nBackend)
-    .use(initReactI18next)
-    .init({
-      fallbackLng: 'en', // мова, яка буде завантажена, якщо переклади, які шукає користувач, будуть недоступні
-      // lng: 'en', // мова за замовчуванням
-      lng: userLanguage, // за замовчуванням мова браузера
-      interpolation: {
-        escapeValue: false, // використовується для екранування значень і уникнення атак XSS
-      },
-      backend: {
-        loadPath: `${getCurrentHost}/i18n/{{lng}}.json`,
-      },
-    });
-}
+i18n
+  .use(i18nBackend)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: 'en', // мова, яка буде завантажена, якщо переклади, які шукає користувач, будуть недоступні
+    // lng: 'en', // мова за замовчуванням
+    lng: userLanguage, // за замовчуванням мова браузера
+    interpolation: {
+      escapeValue: false, // використовується для екранування значень і уникнення атак XSS
+    },
+    backend: {
+      loadPath: `${getCurrentHost}/i18n/{{lng}}.json`,
+    },
+  });
 
 export default i18n;
