42aee3d34573cbc9fbdae3d29236636544e53ea8
Yaroslav <129963434+yaroslavhroshev@users.noreply.github.com>
2023-12-24 19:32:30 +0200

    Start working on the PartnersCard

---
+++ client/src/adminPanelComponents/pages/Partners/styles.js
@@ -0,0 +1,21 @@
+export const stylesBorder = {
+  '& .MuiOutlinedInput-root:hover': {
+    '& > fieldset': {
+      borderColor: 'primary.hover',
+    },
+  },
+};
+
+export const stylesSearch = {
+  color: 'primary.main',
+  bgcolor: 'background.default',
+  width: '100%',
+  minWidth: '260px',
+  height: {
+    mobile: '51px',
+    tablet: '53px',
+    desktop: '60px',
+  },
+};
+
+export const anotherStyle = {};
