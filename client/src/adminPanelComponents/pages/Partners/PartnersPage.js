06825ba726603059880175b22b806ede903f2d21
Yaroslav <129963434+yaroslavhroshev@users.noreply.github.com>
2024-01-19 22:22:31 +0200

    Start working on search logic

--- client/src/adminPanelComponents/pages/Partners/PartnersPage.js
+++ client/src/adminPanelComponents/pages/Partners/PartnersPage.js
@@ -2,27 +2,36 @@ import { Box, Typography, Container, Stack, Autocomplete, TextField, InputAdornm
 import React, { useEffect } from 'react';
 import { shallowEqual, useDispatch, useSelector } from 'react-redux';
 import SearchIcon from '@mui/icons-material/Search';
-import { Link } from 'react-router-dom';
-// import Filter from '../../../components/Filter/Filter';
-// import SectionSwipperFilterSearch from '..
-// /../../components/SectionSwipper&Filter&Search/SectionSwipper&Filter&Search';
+import { Link, useNavigate } from 'react-router-dom';
 import AddBoxOutlinedIcon from '@mui/icons-material/AddBoxOutlined';
 import { fetchAllPartnersNames } from '../../../redux/slices/partnersSlice';
-import { stylesBorder, stylesSearch } from './styles';
-// import ListItem from '../../../components/ListItems/ListItem';
-// import RestaurantItem from '../../../components/RestaurantItem/RestaurantItem';
-// import RestaurantCard from '../../../components/RestaurantCard/RestaurantCard';
+import {
+  stylesBorder,
+  stylesSearch,
+  container,
+  allContentWrapper,
+  searchButtonsTitleWrapper,
+  searchAndButtonsWrapper,
+  searchBtn,
+  addPartnersBtn,
+  title,
+  cardContainer,
+} from './styles';
 import useGetAPI from '../../../customHooks/useGetAPI';
-// import { fixedEncodeURIComponent } from '../../../utils/uriEncodeHelpers';
 import PartnersCard from '../../PartnersCard/PartnersCard';
 import { outlinedBtnStyles, containedBtnStyles } from '../../../muiTheme/buttonsStyles';
 
 const PartnersPage = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   useEffect(() => {
     dispatch(fetchAllPartnersNames());
   }, [dispatch]);
   const [partners, loading] = useGetAPI('/partners');
+
+  const handleAddNewPartners = () => {
+    navigate('/admin-panel/partners/new-partner');
+  };
 
   console.log(partners);
   const itemsFromSearch = useSelector((state) => state.search.search, shallowEqual);
@@ -32,36 +41,16 @@ const PartnersPage = () => {
   console.log(itemsFromSearch);
   return (
     <Container
-      sx={{
-        textAlign: 'center',
-      }}
+      sx={container}
     >
       <Box
-        sx={{
-          width: '100%',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          flexDirection: 'column',
-        }}
+        sx={allContentWrapper}
       >
         <Box
-          sx={{
-            maxWidth: '1083px',
-            width: '100%',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-            py: '40px',
-          }}
+          sx={searchButtonsTitleWrapper}
         >
-          <Box sx={{
-            display: 'flex',
-            width: '100%',
-            gap: '24px',
-            mb: '34px',
-          }}
+          <Box
+            sx={searchAndButtonsWrapper}
           >
             <Stack
               sx={stylesSearch}
@@ -97,18 +86,7 @@ const PartnersPage = () => {
               variant="contained"
               sx={{
                 ...containedBtnStyles,
-                width: '188px',
-                height: '60px',
-                borderRadius: '18px',
-                padding: '12px 16px',
-                marginRight: '5px',
-                fontSize: {
-                  tablet: '1.1em',
-                  desktop: '1.3em',
-                },
-                '@media (min-width: 1212px )': {
-                  fontSize: '1.5em',
-                },
+                ...searchBtn,
               }}
             >
               Search
@@ -116,18 +94,10 @@ const PartnersPage = () => {
             <Button
               variant="outlined"
               endIcon={<AddBoxOutlinedIcon />}
+              onClick={handleAddNewPartners}
               sx={{
                 ...outlinedBtnStyles,
-                height: '60px',
-                width: '248px',
-                padding: '12px 5px',
-                fontSize: {
-                  tablet: '1em',
-                  desktop: '1.12em',
-                },
-                '@media (min-width: 1212px )': {
-                  fontSize: '1.5em',
-                },
+                ...addPartnersBtn,
               }}
             >
               Add Partners
@@ -136,23 +106,13 @@ const PartnersPage = () => {
 
           <Typography
             variant="h1"
-            sx={{
-              fontSize: {
-                mobile: '48px',
-              },
-            }}
+            sx={title}
           >
             Partners
           </Typography>
         </Box>
         <Box
-          sx={{
-            display: 'flex',
-            alignItems: 'center',
-            flexDirection: 'column',
-            gap: '40px',
-            mb: '100px',
-          }}
+          sx={cardContainer}
         >
           {partners !== null && partners.length !== 0 && partners.map(
             ({ imageUrl, enabled, _id, name, customId }) => (
--- client/src/adminPanelComponents/pages/Partners/styles.js
+++ client/src/adminPanelComponents/pages/Partners/styles.js
@@ -19,4 +19,73 @@ export const stylesSearch = {
   },
 };
 
-export const anotherStyle = {};
+export const container = {
+  textAlign: 'center',
+};
+
+export const allContentWrapper = {
+  width: '100%',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  flexDirection: 'column',
+};
+
+export const searchButtonsTitleWrapper = {
+  maxWidth: '1083px',
+  width: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  py: '40px',
+};
+
+export const searchAndButtonsWrapper = {
+  display: 'flex',
+  width: '100%',
+  gap: '24px',
+  mb: '34px',
+};
+
+export const searchBtn = {
+  width: '188px',
+  height: '60px',
+  borderRadius: '18px',
+  padding: '12px 16px',
+  marginRight: '5px',
+  fontSize: {
+    tablet: '1.1em',
+    desktop: '1.3em',
+  },
+  '@media (min-width: 1212px )': {
+    fontSize: '1.5em',
+  },
+};
+
+export const addPartnersBtn = {
+  height: '60px',
+  width: '248px',
+  padding: '12px 5px',
+  fontSize: {
+    tablet: '1em',
+    desktop: '1.12em',
+  },
+  '@media (min-width: 1212px )': {
+    fontSize: '1.5em',
+  },
+};
+
+export const title = {
+  fontSize: {
+    mobile: '48px',
+  },
+};
+
+export const cardContainer = {
+  display: 'flex',
+  alignItems: 'center',
+  flexDirection: 'column',
+  gap: '40px',
+  mb: '100px',
+};
